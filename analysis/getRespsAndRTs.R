f0cd2c77a362a9f628fb6f39f3b826bdadf7c118
Matan <matanmazor@outlook.com>
2023-07-26 14:51:15 +0100

    verification3£

--- analysis/getRespsAndRTs.R
+++ analysis/getRespsAndRTs.R
@@ -7,7 +7,20 @@ groundhog.library(
   ), "2022-12-01"
 )
 
-source("../analysis/loadAndPreprocessData.R")
+E2.df_for_json <- read_csv('../experiments/Exp2rows/data/jatos_resultfiles_batch1/all_data.csv') %>%
+  group_by(subject_identifier,trial_index) %>%
+  filter(trial_type=='noisyLetter' & frame_index==max(frame_index))%>%
+  mutate(RT = as.numeric(RT),
+         nframes=frame_index)  %>%
+  dplyr::select(subject_identifier, RT, nframes, response)
+
+E3.df_for_json <- read_csv('../experiments/Exp3reference/data/jatos_results_files_batch1/all_data.csv') %>%
+
+  group_by(subject_identifier,trial_index) %>%
+  filter(trial_type=='noisyLetter' & frame_index==max(frame_index))%>%
+  mutate(RT = as.numeric(RT),
+         nframes=frame_index)  %>%
+  dplyr::select(subject_identifier, RT, nframes, response)
 
 makeJson <- function(df,filename) {
   
@@ -16,13 +29,14 @@ makeJson <- function(df,filename) {
     summarise(
       RT = list(RT),
       response = list(response),
+      nframes = list(nframes),
       .groups = "drop"  # drop the grouping
     )
   
   list_df <- split(df_grouped[setdiff(names(df_grouped), "subject_identifier")], df_grouped$subject_identifier)
   
   named_list <- setNames(lapply(list_df, function(x) {
-    list(RT = unlist(x$RT), response = unlist(x$response))
+    list(RT = unlist(x$RT), response = unlist(x$response), nframes = unlist(x$nframes))
   }), unique(df_grouped$subject_identifier))
   
   json_output <- toJSON(named_list, auto_unbox = TRUE)
@@ -30,5 +44,7 @@ makeJson <- function(df,filename) {
   write(json_output, filename)
   
 }
+
+makeJson(E2.df_for_json,'../experiments/Exp2rows/data/responses.json')
   
   
